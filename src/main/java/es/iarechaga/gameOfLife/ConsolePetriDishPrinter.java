80159dbd1b6b7a71ea9b94c0e30795107e36102e
Ignacio Arechaga <iarechaga@es.amadeus.com>
2017-08-06 17:38:06 +0200

    Print into the console the full petri dish

---
+++ src/main/java/es/iarechaga/gameOfLife/ConsolePetriDishPrinter.java
@@ -0,0 +1,27 @@
+package es.iarechaga.gameOfLife;
+
+import es.iarechaga.gameOfLife.storage.CellBlock;
+import es.iarechaga.gameOfLife.storage.SquaredPetriDish;
+
+public class ConsolePetriDishPrinter {
+
+    private static final String LIVING_CELL_SYMBOL = "#";
+    private static final String EMPTY_CELL_SYMBOL = "O";
+
+    public void print(SquaredPetriDish dish) {
+        Integer range = dish.getRange();
+
+        for (int row = 0; row < range; row++) {
+            for (int column = 0; column < range; column++) {
+                CellBlock cell = dish.getCell(row, column);
+                if (cell.containsLivingCell()) {
+                    System.out.print(LIVING_CELL_SYMBOL);
+                } else {
+                    System.out.print(EMPTY_CELL_SYMBOL);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
